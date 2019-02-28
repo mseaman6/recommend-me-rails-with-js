d07497b22bb6767f2b09605bed72ad69aee26416
mseaman6 <monicaameeti@gmail.com>
2019-02-28 11:23:40 -0500

    updates to recommendation serializer

---
+++ app/serializers/recommendation_serializer.rb
@@ -0,0 +1,7 @@
+class RecommendationSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :user_id, :category_id
+  belongs_to :user
+  belongs_to :category
+  has_many :comments
+  has_many :commentors, :class_name => 'User', through: :comments
+end
