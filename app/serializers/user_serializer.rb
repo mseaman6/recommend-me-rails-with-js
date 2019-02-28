527403693224fdafc95656afd53073b1a7b09ae2
mseaman6 <monicaameeti@gmail.com>
2019-02-28 11:25:16 -0500

    updates to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :password, :password_confirmation
+
+  has_many :recommendations
+  has_many :categories, through: :recommendations
+  has_many :comments
+  has_many :commented_recommendations, :through => :comments, :source => :recommendation
 end
