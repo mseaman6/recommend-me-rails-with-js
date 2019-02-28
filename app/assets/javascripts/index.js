14f6d923a7d1047b143f78bc0f03497d778dbe10
mseaman6 <monicaameeti@gmail.com>
2019-02-28 12:42:27 -0500

    create index.js and create function to get JSON rec data

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,36 @@
+$(function() {
+  console.log("index.js loaded...");
+})
+
+
+function getRecommendations() {
+  $.ajax({
+    url: '/recommendations', //'https://localhost:3---/recommendations' or onclick of a certain button, etc.
+    method: 'get',
+    dataType: 'json'
+  }).done(function (response) {
+    console.log("index.js response: ", response);
+
+    let rec = new Recommendation(response[0])
+    //switch to .map functionality to process all itmes in response array
+
+    let recHtmlData = rec.recHTML()
+
+    //append recHtmlData to DOM, etc.
+    debugger;
+  })
+}
+
+class Recommendation {
+  constructor(obj) {
+    this.title = obj.title
+    this.description = obj.description
+    this.user_id = obj.user_id
+  }
+}
+
+Recommendation.prototype.recHTML = function () {
+  return ('
+    <div>${this.title}</div>
+  ')
+}
